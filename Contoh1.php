58307ac183d9d089fd0b0818de41b9199a65324b
iHamzeL <145653383+iHamzeL@users.noreply.github.com>
2023-10-05 19:17:23 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ilham Larang Priana
+            Saya tinggal di daerah Bogor utara lebih tepatnya Bantarjati Lebak
+            olahraga yang saya sukai adalah bersepeda";
+    }
+}
